05e49fff2c85e43bc6a91cc3188dc6ff4869b479
Asir Ahbab Raiyan <ahbab.raiyan@gmail.com>
2025-05-08 14:46:54 +0600

    Social Contacts Added.

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -1,5 +1,6 @@
 import SingleContactSocial from "./SingleContactSocial";
-import { FaFacebook, FaKaggle, FaLinkedin } from "react-icons/fa";
+import { FaCodeBranch, FaFacebook, FaKaggle, FaLinkedin } from "react-icons/fa";
+import { FaCode, FaHackerrank } from "react-icons/fa6";
 import { FiGithub } from "react-icons/fi";
 
 const ContactSocial = () => {
@@ -9,6 +10,8 @@ const ContactSocial = () => {
       <SingleContactSocial link="https://www.kaggle.com/asirahbabraiyan" Icon={FaKaggle} />
       <SingleContactSocial link="https://github.com/asirahbab01" Icon={FiGithub} />
       <SingleContactSocial link="https://www.facebook.com/asirahbab.raiyan" Icon={FaFacebook} />
+      <SingleContactSocial link="https://www.hackerrank.com/profile/asir_ahbabraiyan" Icon={FaHackerrank} />
+      <SingleContactSocial link="https://www.codechef.com/users/ahbabraiyan200" Icon={FaCode} />
     </div>
   );
 };
